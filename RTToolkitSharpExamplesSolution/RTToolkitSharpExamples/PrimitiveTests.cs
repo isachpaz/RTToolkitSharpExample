b196cf93383c90aea1f03e9a29e6a512be4d8e8b
Dr. Ilias Sachpazidis <ilias.sachpazidis@gmail.com>
2020-02-24 01:12:47 +0100

    PrimitiveTests added

---
+++ RTToolkitSharpExamplesSolution/RTToolkitSharpExamples/PrimitiveTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using RTToolkitSharp.DoseVolumeHistogramLib.Factory;
+using RTToolkitSharp.DoseVolumeHistogramLib.Helpers;
+using RTToolkitSharp.RTQuantities.Helpers;
+using RTToolkitSharp.RTQuantities.Quantities.Dose;
+using RTToolkitSharp.RTQuantities.Quantities.Volume;
+using Xunit;
+
+namespace RTToolkitSharpExamples
+{
+    public class PrimitiveTests
+    {
+        [Fact]
+        public void PhysicalDose_Addition_Test()
+        {
+            var dosis = RangeHelper<XDouble>.Range(0, 1000, 0.001).ToList();
+            var xs = Enumerable.Range(1, dosis.Count);
+
+            foreach (var t in dosis.Zip(xs, (d, i) => Tuple.Create(d.Value, i)))
+            {
+                var sum = PhysicalDoseValue.InGy(t.Item1 + t.Item2) + PhysicalDoseValue.InGy(t.Item2);
+                var sum1 = PhysicalDoseValue.InGy(t.Item1 + 2 * t.Item2);
+                Assert.Equal(sum, sum1);
+            }
+        }
+
+        [Fact]
+        public void PhysicalDose_Multiplication_Test()
+        {
+            var dosis = RangeHelper<XDouble>.Range(0, 2, 0.000001);
+            var xs = Enumerable.Range(1, 100000000);
+
+            foreach (var t in dosis.Zip(xs, (d, i) => Tuple.Create(d.Value, i)))
+            {
+                var sum = PhysicalDoseValue.InGy(t.Item1) * t.Item1;
+                var sum1 = PhysicalDoseValue.InGy(Math.Round(t.Item1* t.Item1, 9));
+                Assert.Equal(sum, sum1);
+            }
+        }
+
+    }
+}
---
+++ RTToolkitSharpExamplesSolution/RTToolkitSharpExamples/XDouble.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using RTToolkitSharp.RTQuantities.Interfaces;
+
+namespace RTToolkitSharpExamples
+{
+    internal struct XDouble : IQuantityCreation<XDouble>
+    {
+        public XDouble(double value)
+        {
+            Value = value;
+        }
+        public double Value { get; private set; }
+        public double GetValue() => Value;
+        public string GetUnit()=> String.Empty;
+        public XDouble TNew(double value, string unit) => new XDouble(value);
+
+        public static implicit operator XDouble(double dValue)
+        {
+            return new XDouble(dValue);
+        }
+    }
+}
